f0472e720778af3653c377b36148a79c901e7999
domanskidawid <domanski.dawid@gmail.com>
2025-01-29 16:15:40 +0100

    a1

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // index.js
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
     // You can log the event to inspect it, if necessary.
     console.log("Received event:", JSON.stringify(event, null, 2));
-    
+  
     const response = {
       statusCode: 200,
       body: JSON.stringify({
         message: "Hello, World!",
       }),
     };
-    
+  
     return response;
   };
   
